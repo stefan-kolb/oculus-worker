cf0d5dec31faef690eb6b4cb9ac24fa3c37d1270
stefan-kolb <stefan-kolb@web.de>
2014-11-19 00:57:47 +0100

    Fixes Java update number

--- modules/java.rb
+++ modules/java.rb
@@ -31,7 +31,7 @@ class Java
     text = download
     versions = text.scan /java\sSE\sDevelopment\sKit\s([0-9]+u[0-9]+)/i
     flat = versions.inject([]) { |arr, obj| arr << obj[0] }.compact.uniq
-    flat.collect! { |e| '1.' << e.split('u')[0] << '.0_' << e.split('u')[0] }
+    flat.collect! { |e| '1.' << e.split('u')[0] << '.0_' << e.split('u')[1] }
     @versions = flat.collect! { |e| Versionomy.parse(e) }
   end
 end
